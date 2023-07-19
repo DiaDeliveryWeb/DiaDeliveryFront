49284b968965ab1b03b192d7bffafda38d5a2d82
sxi8022 <sxi8022@gmail.com>
2023-07-19 16:51:03 +0900

    - 불필요소스코드 수정

--- src/main/java/com/dia/deliveryfront/user/controller/UserController.java
+++ src/main/java/com/dia/deliveryfront/user/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.dia.deliveryfront.user.controller;
 
+import com.dia.deliveryfront.user.dto.AuthRequestDto;
+import com.dia.deliveryfront.user.dto.SignUpRequestDto;
 import com.dia.deliveryfront.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -34,6 +41,35 @@ public class UserController {
     @GetMapping("/profile")
     public String profilePage() {
         return "user/profile";
+    }
+
+    /*
+     * 회원 가입
+     *
+     */
+    @PostMapping("/signup")
+    public String signup(@Valid SignUpRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/user/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/user/login-page";
+    }
+
+    // 회원 관련 정보 받기 토큰을 서버로 넘겨야함
+    @GetMapping("/user-info")
+    @ResponseBody
+    public AuthRequestDto getUserInfo(String auth) {
+        AuthRequestDto responseDto = userService.getUserInfo(auth);
+
+        return responseDto;
     }
 
 }
