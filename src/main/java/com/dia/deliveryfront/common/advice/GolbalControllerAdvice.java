a4d5ab3becc57afbaabcb0e2bba9817096e127c5
sxi8022 <sxi8022@gmail.com>
2023-07-19 16:29:39 +0900

    - server.back.address 속성 추가 - 로그인 인증 절차 없앰 - 소스 프로그램 정리

--- src/main/java/com/dia/deliveryfront/common/advice/GolbalControllerAdvice.java
+++
@@ -1,8 +0,0 @@
-package com.dia.deliveryfront.common.advice;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GolbalControllerAdvice {
-
-}
---
+++ src/main/java/com/dia/deliveryfront/order/controller/RestOrderController.java
@@ -0,0 +1,11 @@
+package com.dia.deliveryfront.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class RestOrderController {
+}
