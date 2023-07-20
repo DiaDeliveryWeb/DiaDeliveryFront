fd0ce4efd526a0eca3b5515f3fd941f216f5a1d1
yesrin <asjs11@naver.com>
2023-07-20 20:20:14 +0900

    api 요청 타입 변경전 중간저장

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -22,19 +22,26 @@ function openTab(tabName) {
     event.currentTarget.classList.add("active");
 }
 
-
 $(document).ready(function () {
     // jQuery to handle the cart functionality
     const cartItems = [];
 
     // Event handler for the "담기" button
     $(".add-to-cart-button").on("click", function () {
-        const productId = $(this).siblings("span[name='productId']").text();
-        addToCart(productId);
+        const $menuItem = $(this).closest(".menu-item");
+        const productId = $menuItem.find("span[name='productId']").text();
+        const productName = $menuItem.find("h3[name='productName']").text();
+        const price = $menuItem.find("span[name='price']").text();
+
+        addToCart(productId, productName, price);
     });
 
-    function addToCart(productId) {
-        cartItems.push(productId);
+    function addToCart(productId, productName, price) {
+        cartItems.push({
+            productId: productId,
+            productName: productName,
+            price: price
+        });
         updateCart();
     }
 
@@ -42,8 +49,8 @@ $(document).ready(function () {
         const cartList = $("#cart-items");
         cartList.empty(); // Clear previous cart items
 
-        cartItems.forEach(productId => {
-            const li = $("<li>").text(productId);
+        cartItems.forEach(item => {
+            const li = $("<li>").text(item.productName + " " + item.price);
             cartList.append(li);
         });
     }
@@ -51,17 +58,26 @@ $(document).ready(function () {
     // Function to send API request with selected product IDs
     function sendCartToApi() {
         if (cartItems.length > 0) {
+            // Extract only the productIds from the cartItems array
+            const productIds = cartItems.map(item => item.productId);
+
             // Implement your API request logic here
             // You can use jQuery's $.ajax() or $.post() for the AJAX request
-            $.post("YOUR_API_ENDPOINT", JSON.stringify(cartItems))
+            $.post(otherHost+"/orders", JSON.stringify(productIds))
                 .done(function (data) {
                     // Handle API response if needed
+                    console.log("Order placed successfully!");
                 })
                 .fail(function (error) {
-                    console.error("Error sending cart data to API:", error);
+                    console.error("Error placing order:", error);
                 });
         }
     }
+
+    // Attach click event handler using jQuery
+    $(".order-button").on("click", function () {
+        sendCartToApi();
+    });
 
     // Add other functions or code relevant to the order page here
 
