26257835e36221a7411b94e27ce07715452f79b1
sxi8022 <sxi8022@gmail.com>
2023-07-20 15:13:59 +0900

    - 가게등록 페이지 저장(개발중)

--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -1,3 +1,169 @@
 $(document).ready(function () {
 
-})
+    // This button will increment the value
+    $('.qty_plus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // 행추가
+        rowAdd();
+
+
+    });
+    // This button will decrement the value till 0
+    $(".qty_minus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // 행 삭제
+        rowDelete();
+    });
+});
+
+function doStoreSave() {
+
+
+    var store = {
+        name: "",
+        category: "",
+        introduction: "",
+        imageUrl: "",
+        productList: []
+    };
+
+    if ($('#st_name').val() == "") {
+        alert("가게명을 입력하세요.");
+        return false;
+    }
+    if ($('#st_category').val() == "") {
+        alert("카테고리를 입력하세요.");
+        return false;
+    }
+    if ($('#st_introduction').val() == "") {
+        alert("가게 소개를 입력하세요.");
+        return false;
+    }
+
+    //FormData 새로운 객체 생성
+    var formData = new FormData();
+    //storeImage
+    // input class 값
+    var storeFileInput = $('#AddImgs');
+
+    store.name = $('#st_name').val();
+    store.category = $('#st_category').val();
+    store.introduction = $('#st_introduction').val();
+
+    for (var i = 0; i < storeFileInput.length; i++) {
+        if (storeFileInput[i].files.length > 0) {
+            // formData에 'file'이라는 키값으로 fileInput 값을 append 시킨다.
+            formData.append('storeImage', storeFileInput.files[i]);
+        }
+    }
+    debugger;
+    $('#tbProductList tr').each(function() {
+        var product = {
+            imageUrl: "",
+            productName: "",
+            price: 0,
+            description: ""
+        };
+        var tr = $(this);
+        var td = tr.children();
+
+
+        if (td.eq(3).find('input').val() == "") {
+            alert('상품명을 입력하세요.');
+            return false;
+        }
+        if (td.eq(4).find('input').val() == "") {
+            alert('가격을 입력하세요.');
+            return false;
+        }
+        if (td.eq(5).find('input').val() == "") {
+            alert('설명을 입력하세요.');
+            return false;
+        }
+
+        product.productName = td.eq(3).find('input').val(); // 상품명
+        product.price = td.eq(4).find('input').val(); // 가격
+        product.description = td.eq(5).find('input').val(); // 설명
+
+        store.productList.push(product);
+    });
+    // 요청 데이터 전달
+    formData.append('requestDto', new Blob([ JSON.stringify(store) ], {type : "application/json"}));
+    debugger;
+    // input class 값
+    var productFileInput = $('.products-upload-control');
+    // fileInput 개수를 구한다.
+    for (var i = 0; i < productFileInput.length; i++) {
+        if (productFileInput[i].files.length > 0) {
+            for (var j = 0; j < productFileInput[i].files.length; j++) {
+                console.log(" productFileInput[i].files[j] :::"+ productFileInput[i].files[j]);
+
+                // formData에 'file'이라는 키값으로 fileInput 값을 append 시킨다.
+                formData.append('productsImage', $('.products-upload-control')[i].files[j]);
+            }
+        }
+    }
+    debugger;
+    $.ajax({
+        type: "POST",
+        url: (otherHost  + `/stores`),
+        data: formData,
+        contentType: false,               // * 중요 *
+        processData: false,               // * 중요 *
+        enctype : 'multipart/form-data'  // * 중요 *
+    })
+        .done(function (res, status, xhr) {
+            alert("저장에 성공하였습니다.");
+            window.location.href = host;
+        })
+        .fail(function (jqXHR, textStatus) {
+            alert("Login Fail");
+        });
+
+}
+
+// 이전페이지로 돌아감
+function onCancel() {
+    location.href = host;
+    return;
+}
+
+
+function rowAdd() {
+    var trCnt = $('#tbProductList tr').length;
+    if (trCnt < 5) {
+        var innerHtml = "";
+        var tmpId = "AddImgs_" + (trCnt + 1);
+        var tmpPreviewId = "Preview_" + (trCnt + 1);
+        innerHtml += '<tr style="font-size:12px; font-weight:400;">';
+        innerHtml += '<td style="text-align:center;">' + (trCnt + 1) + '</td>';
+        innerHtml += '<td>' + '<div class="fileBox clearfix">';
+        innerHtml += '<div class="inputFile">';
+        innerHtml += '<label for="' + tmpId + '" class="addImgBtn">' + "+";
+        innerHtml += '</label>'
+        innerHtml += '<input type="file" id="' + tmpId +'" class="upload-hidden products-upload-control" accept=".jpg, .png, .gif" multiple />'
+        innerHtml += '<ul id="'+ tmpPreviewId + '" class="sortable"></ul>'
+        innerHtml += '</div>';
+        innerHtml += '</div>' + '</td>';
+        innerHtml += '<td><input type="text" name="productName" placeholder="상품이름"></td>';
+        innerHtml += '<td><input type="text" name="price" placeholder="가격"></td>';
+        innerHtml += '<td><input type="text" name="description" placeholder="설명"></td>';
+        innerHtml += '</tr>';
+
+        $('#products_table > tbody:last').append(innerHtml);
+    } else {
+        alert("최대 5개까지만 가능합니다.");
+        return false;
+    }
+}
+
+function rowDelete() {
+    var trCnt = $('#tbProductList tr').length;
+    if (trCnt > 1) {
+        $('#products_table > tbody:last > tr:last').remove();
+    } else {
+        return false;
+    }
+}
